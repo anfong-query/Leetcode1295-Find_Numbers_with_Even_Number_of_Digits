8d2641c290b5d32f0f3cc6362a6e17cc5d575f67
anfong-query <71242441+anfong-query@users.noreply.github.com>
2021-11-14 12:04:11 +0800

    Update Leetcode1295.c

--- Leetcode1295.c
+++ Leetcode1295.c
@@ -1,21 +1,21 @@
 void _start(){	
-	int numsSize = 7;
-	int ans = 0;
-	int nums[] = {12,345,2,6,7896,1212,12345};
+    int numsSize = 7;
+    int ans = 0;
+    int nums[] = {12,345,2,6,7896,1212,12345};
 
     for(int i = 0 ; i < numsSize ; ++i){
         int j = 0;
         int tmp = nums[i];
         while(tmp > 9){
-            //tmp /= 10;
-           	int q,r;
-           	q = (tmp>>1)+(tmp>>2);
-           	q = q+(q>>4);
-           	q = q+(q>>8);
-           	q = q+(q>>16);
-			q = q >> 3;
-			r = tmp-(((q<<2)+q) << 1);
-			tmp = q + (r>9);
+	    //tmp /= 10;
+            int q,r;
+            q = (tmp>>1)+(tmp>>2);
+            q = q+(q>>4);
+            q = q+(q>>8);
+            q = q+(q>>16);
+	    q = q >> 3;
+	    r = tmp-(((q<<2)+q) << 1);
+	    tmp = q + (r>9);
             j ++;
         }
         j ++;
